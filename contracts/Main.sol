2209820aac5b3cbf20352bce543853e660f095ab
chamodya <chamodyawimansha@gmail.com>
2019-02-25 15:42:51 +0530

    before changing to react template

---
+++ contracts/Main.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.5.0;
+
+contract Main{
+
+    address private messagesContract;
+    address private usersContract;
+    address private postsContract;
+
+    constructor() public{
+
+    }
+
+    
+
+}
